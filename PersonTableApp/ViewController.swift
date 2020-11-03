f0f9d28c1c6ef860f5c9f7a40e710c0beb5fc0f1
Pavel Grishanin <pgrishanin@gmail.com>
2020-11-03 09:58:27 +0300

    Initial Commit

---
+++ PersonTableApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonTableApp
+//
+//  Created by Pavel Grishanin on 03.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
